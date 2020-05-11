6a935748305fc11f77a777304333a93d2a5e4593
Wayne Lee <yuckar97@gmail.com>
2020-05-11 21:43:25 +0800

    Changelog - Added functionality to Withdraw function

--- Prison/src/com/Prison/commands/CmdWithdraw.java
+++ Prison/src/com/Prison/commands/CmdWithdraw.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -19,10 +20,28 @@ public class CmdWithdraw implements CommandExecutor {
 				return true;
 			}
 			if(args.length == 1) {
-				ItemStack item = new ItemStack(Material.PAPER, 1);
-				ItemMeta meta = item.getItemMeta();
-				meta.setDisplayName(ChatColor.GREEN + " $500");
-				item.setItemMeta(meta);
+				
+				//Get the players inventory data
+				Player p = (Player) sender;
+				Inventory inv = p.getInventory();
+				
+				//Check for full inventory
+				if(inv.firstEmpty() == -1) 
+				{
+					p.sendMessage("INVENTORY IS FULL");
+				}
+				//if inventory has space
+				else
+				{
+					//Create the Cheque Item
+					ItemStack item = new ItemStack(Material.PAPER, 1);
+					ItemMeta meta = item.getItemMeta();
+					meta.setDisplayName(ChatColor.GREEN + " $500");
+					item.setItemMeta(meta);
+					
+					//add the withdrawn cheque
+					inv.addItem(item);			
+				}
 				return true;
 			} else {
 				//too many args
