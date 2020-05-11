6d3f1a5d888da62b27674be1670c80b06edc4129
Wei En <dennislingweien@gmail.com>
2020-05-12 02:12:32 +0800

    Per Player File created.

--- Prison/src/com/Prison/listener/PlayerJoin.java
+++ Prison/src/com/Prison/listener/PlayerJoin.java
@@ -1,23 +1,53 @@
 package com.Prison.listener;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import com.Prison.main.Main;
+
 import net.md_5.bungee.api.ChatColor;
 
 public class PlayerJoin implements Listener {
 
+	private Main main;
+	public PlayerJoin(Main main) {
+		this.main = main;
+	}
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
+		
 		Player p = e.getPlayer();
+		setup(p);
 		if(!p.hasPlayedBefore()) {
 			Bukkit.broadcastMessage(ChatColor.DARK_GREEN + " + " + ChatColor.GRAY + p.getName());
-
 		}
 	}
+	
+	public void setup(Player p) {
+		String uuid = p.getUniqueId().toString();
+		File file = new File(main.getDataFolder() + "/Player/"+ uuid + ".yml");	
+		if(!file.exists()) {
+			try {
+				file.createNewFile();
+				p.sendMessage("File created");
+			} catch (IOException e) {
+				e.printStackTrace();
+				p.sendMessage("Error creating");
+			}
+		}
+		else {
+			p.sendMessage("file already created!");
+		}
+	}
+	
 
+	
+	
 	
 }
