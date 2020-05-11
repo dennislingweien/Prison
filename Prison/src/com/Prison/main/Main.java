00741614e1973964338ab6be1ac58fa110d78b29
Wei En <dennislingweien@gmail.com>
2020-05-11 15:32:25 +0800

    Auto stash before merge of "master" and "origin/master"

--- Prison/src/com/Prison/main/Main.java
+++ Prison/src/com/Prison/main/Main.java
@@ -1,5 +1,21 @@
 package com.Prison.main;
 
 public class Main {
+<<<<<<< Updated upstream
 
+=======
+	public void onEnable() {
+		registerCommands();
+		registerListeners();
+	}
+	public void onDisable() {
+		
+	}
+	public void registerCommands() {
+		
+	}
+	public void registerListeners() {
+		
+	}
+>>>>>>> Stashed changes
 }
