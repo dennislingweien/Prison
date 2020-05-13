cd7c2f67af20b61b79023d1d327d32b918bac381
Wei En <dennislingweien@gmail.com>
2020-05-14 03:53:07 +0800

    PlayerChat test

--- Prison/src/com/Prison/listener/CustomPickaxe.java
+++ Prison/src/com/Prison/listener/CustomPickaxe.java
@@ -3,5 +3,9 @@ package com.Prison.listener;
 import org.bukkit.event.Listener;
 
 public class CustomPickaxe implements Listener {
-
+	
+	
+	
+	
+	
 }
--- Prison/src/com/Prison/listener/PlayerChat.java
+++ Prison/src/com/Prison/listener/PlayerChat.java
@@ -1,5 +1,6 @@
 package com.Prison.listener;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,7 +10,9 @@ public class PlayerChat implements Listener {
 
 	@EventHandler
 	public void onPlayerChat(AsyncPlayerChatEvent e) {
-		//e.setMessage("");
-		//Player p = (Player) e.getPlayer();
+		String rank = "&4[OWNER]"; 
+		String tag = "&7&l[&b&lFrosty&7&l]";
+		Player p = (Player) e.getPlayer();
+		e.setMessage(ChatColor.translateAlternateColorCodes('&', rank + " " + tag + p.getName() + ":" + e.getMessage()));
 	}
 }
